53ea84b3c26baba0731e8223c22a67c3799a9d5f
unknown <kewbitxmr@protonmail.com>
2024-12-08 19:38:34 +0000

    deps

--- lib/models/haveno/p2p/haveno_seednode.dart
+++ lib/models/haveno/p2p/haveno_seednode.dart
@@ -16,7 +16,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import 'package:haveno_app/models/haveno/p2p/haveno_network';
+import 'package:haveno_app/models/haveno/p2p/haveno_network.dart';
 
 class HavenoSeedNode {
   final String onionHost;
